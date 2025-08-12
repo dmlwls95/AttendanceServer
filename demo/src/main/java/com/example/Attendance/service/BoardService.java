0b2c43c4cc8c292530622e72fe9e719a59e3fa2d
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2025-08-12 12:46:19 +0900

    BoardService에 Notification 정보 전송
    notification 정보를 전송하는 코드 추가

--- demo/src/main/java/com/example/Attendance/service/BoardService.java
+++ demo/src/main/java/com/example/Attendance/service/BoardService.java
@@ -20,10 +20,14 @@ import javax.transaction.Transactional;
 public class BoardService {
 
     private final BoardRepository boardRepository;
+    private final NotificationService notificationService;
 
     public void write(BoardDTO dto) {
         Board entity = toEntity(dto);
         boardRepository.save(entity);
+        
+        try {notificationService.createNotification(entity);} 
+        catch (Exception e) {e.printStackTrace();}
     }
 
     public Page<BoardDTO> getListByType(String type, Pageable pageable) {
