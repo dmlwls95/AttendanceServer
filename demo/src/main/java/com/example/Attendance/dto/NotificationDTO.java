99c0060fa16fa48a6245239e4896419acea745ca
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2025-08-12 11:10:59 +0900

    Notification DTO 작성자 추가

--- demo/src/main/java/com/example/Attendance/dto/NotificationDTO.java
+++ demo/src/main/java/com/example/Attendance/dto/NotificationDTO.java
@@ -11,4 +11,5 @@ public class NotificationDTO {
 	private String title;											// board 의 title	[제목]
 	private String writeDate;									// board의 writeDate [작성일]
 	private String message = "새로운 공지 업데이트";		// 알람 메세지 ["새로운 공지 업데이트"]
+	private String writer;
 }
