ef40c4335cc533225175f8493f126ea2baf6889c
dmlwls <dmlwls95@naver.com>
2025-08-13 11:27:31 +0900

    웹소켓 sockjs제외하고 순수 웹소켓으로 전환

--- demo/src/main/java/com/example/Attendance/config/WebSocketConfig.java
+++ demo/src/main/java/com/example/Attendance/config/WebSocketConfig.java
@@ -23,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS(); // STOMP over WebSocket 엔드포인트를 등록
+                .setAllowedOriginPatterns("*");
+                //.withSockJS(); // STOMP over WebSocket 엔드포인트를 등록
 	}  
 }
