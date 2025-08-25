f95b7de0cd4caf81091e51219132e0dffccf9876
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2025-08-26 02:17:48 +0900

    컨트롤러에 알림 삭제 메소드 추가

--- demo/src/main/java/com/example/Attendance/controller/NotificationController.java
+++ demo/src/main/java/com/example/Attendance/controller/NotificationController.java
@@ -1,12 +1,15 @@
 package com.example.Attendance.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Attendance.service.NotificationStatusService;
+import com.example.Attendance.dto.NormalResponse;
+import com.example.Attendance.service.NotificationService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +20,7 @@ import java.security.Principal;
 @RequestMapping("/notification")
 public class NotificationController {
 	
+	private final NotificationService notificationService;
 	private final NotificationStatusService notificationStatusService;
 	
 	@PostMapping("/{notiId}/read")
@@ -25,5 +29,11 @@ public class NotificationController {
         notificationStatusService.readNotification(notiId, userLoginId);
         return ResponseEntity.ok().build();
     }
+	
+	@DeleteMapping("/{notiId}/delete")
+	public boolean deleteNotification(@PathVariable Long notiId) {
+		System.out.println("삭제 함수");
+		return notificationService.deleteNotification(notiId);
+	}
 	
 }
