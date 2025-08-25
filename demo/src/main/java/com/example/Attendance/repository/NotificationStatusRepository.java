b2e13699b9af0c0e2474607a6b929bcfe1c98147
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2025-08-26 02:18:34 +0900

    알림 삭제를 위한 쿼리문 메소드 구현

--- demo/src/main/java/com/example/Attendance/repository/NotificationStatusRepository.java
+++ demo/src/main/java/com/example/Attendance/repository/NotificationStatusRepository.java
@@ -1,6 +1,10 @@
 package com.example.Attendance.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.example.Attendance.entity.NotificationStatus;
 import com.example.Attendance.entity.User;
 import com.example.Attendance.entity.Notification;
@@ -11,5 +15,10 @@ import java.lang.*;
 public interface NotificationStatusRepository extends JpaRepository<NotificationStatus, Long>{
 
 	Optional<NotificationStatus> findByNotificationAndUsers(Notification notification, User user);
-
+	
+	
+	@Modifying
+	@Query("DELETE FROM NotificationStatus ns WHERE ns.notification.notiId = :notiId")
+	void deleteAllById(@Param("notiId") Long notiId);
+	
 }
