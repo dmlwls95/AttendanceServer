229aadde68cf5df1a6c4770b4865960711193261
harib0h23 <harib0h@naver.com>
2025-09-07 15:42:59 +0900

    유저삭제 기능 구현

--- demo/src/main/java/com/example/Attendance/repository/UserRepository.java
+++ demo/src/main/java/com/example/Attendance/repository/UserRepository.java
@@ -19,8 +19,18 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	Optional<User>	findByEmpnum(String empnum);
 	boolean existsByEmail(String email);
 	boolean existsByEmpnum(String empnum);
-	int deleteByEmpnum(String empnum);
-	int countByRole(Role role);
+	//int deleteByEmpnum(String empnum);
+	long countByRoleAndIsDeleted(User.Role role, User.Delete isDeleted);
+
+	
+	// Soft Delete
+	@Query("SELECT u FROM User u WHERE u.isDeleted = 'NOTDELETE'")
+    List<User> findAllByIsDeleted(User.Delete isDeleted);
+    Page<User> findAllByIsDeleted(User.Delete isDeleted, Pageable pageable);
+
+	Optional<User> findByEmailAndIsDeleted(String email, User.Delete isDeleted);
+	Optional<User> findByEmpnumAndIsDeleted(String empnum, User.Delete isDeleted);
+
 	
 	// 오늘 상태 계산: Attendance / Leave 를 LEFT JOIN
 	@Query(
@@ -42,7 +52,7 @@ public interface UserRepository extends JpaRepository<User, Long>{
 		    "from User u " +
 		    "left join Attendance a on a.user = u and a.date = :date " +
 		    "left join Leave l on l.user = u and :date between l.startDate and l.endDate " +
-		    "where u.role = 'USER'"
+		    "where u.role = 'USER' and u.isDeleted = 'NOTDELETE'"
 		)
 	Page<WorkingRowDTO> findWorkingList(@Param("date") LocalDate date, Pageable pageable);
 	
@@ -67,7 +77,8 @@ public interface UserRepository extends JpaRepository<User, Long>{
 		    "left join Attendance a on a.user = u and a.date = :date " +
 		    "left join Leave l on l.user = u and :date between l.startDate and l.endDate " +
 		    "where u.role = 'USER' " +
-		    "  and ( " +
+		    "and u.isDeleted = 'NOTDELETE'"+
+		    "and ( " +
 		    "       :status is null " +
 		    "    or (:status = 'LEAVE'   and l.id is not null) " +
 		    "    or (:status = 'ABSENT'  and l.id is null and a.id is null) " +
@@ -80,4 +91,6 @@ public interface UserRepository extends JpaRepository<User, Long>{
 		    @Param("status") String status, // PRESENT | LEFT | ABSENT | LEAVE (대문자 권장)
 		    Pageable pageable
 		);
+	
+
 }
